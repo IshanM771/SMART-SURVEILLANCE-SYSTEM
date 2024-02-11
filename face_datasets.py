e83f6f50610c751f24d920d211c2764397512edd
IshanM771 <123454910+IshanM771@users.noreply.github.com>
2024-02-11 14:59:21 +0530

    Update face_datasets.py

--- face_datasets.py
+++ face_datasets.py
@@ -75,7 +75,8 @@ cv2.destroyAllWindows()
 
 
 
-##
+# face_dataset.py
+
 import cv2
 import os
 import numpy as np
@@ -85,12 +86,12 @@ def assure_path_exists(path):
     if not os.path.exists(dir):
         os.makedirs(dir)
 
-def collect_training_data(face_id):
+def collect_training_data(name):
     vid_cam = cv2.VideoCapture(0)
     face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     count = 0
 
-    assure_path_exists(f"training_data/{face_id}/")
+    assure_path_exists(f"training_data/{name}/")
 
     while True:
         _, image_frame = vid_cam.read()
@@ -117,7 +118,7 @@ def collect_training_data(face_id):
 
             cv2.rectangle(image_frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
             count += 1
-            cv2.imwrite(f"training_data/{face_id}/person_{face_id}_{count}.jpg", gray[y:y + h, x:x + w])
+            cv2.imwrite(f"training_data/{name}/person_{name}_{count}.jpg", gray[y:y + h, x:x + w])
             cv2.imshow('frame', image_frame)
 
         if cv2.waitKey(100) & 0xFF == ord('q'):
@@ -127,7 +128,3 @@ def collect_training_data(face_id):
 
     vid_cam.release()
     cv2.destroyAllWindows()
-
-# Change the face_id accordingly for each person
-collect_training_data(face_id=1)
-
